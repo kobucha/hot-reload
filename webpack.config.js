1e9ebaa46b97657a2778f1599d63d7c00d6203e3
kobucha <kashikomi@outlook.com>
2022-05-19 19:35:50 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+  context: __dirname + '/app',
+  entry: './entry',
+  output: {
+    path: __dirname + '/public/javascripts',
+    filename: 'bundle.js',
+  },
+  mode: 'none',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  // ここから追加
+  watchOptions: {
+    aggregateTimeout: 200,
+    poll: 1000
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public/javascripts'),
+    },
+    hot: true,
+  },
+  // ここまで
+
+  resolve: {
+    fallback: {
+      buffer: require.resolve('buffer'),
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify')
+    }
+  }
+}
